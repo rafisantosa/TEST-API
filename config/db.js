45f7ec05e229c21fdc166e363a6213812454e90f
Rafi Sans <rafisantosa643@gmail.com>
2025-10-16 11:24:26 +0700

    fix: perbaikan koneksi MySQL dan install dotenv

--- config/db.js
+++ config/db.js
@@ -1,6 +1,21 @@
-module.exports ={
-    host :"localhost",
-    user:"root",
-    password:"",
-    database :"magang",
-}
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Koneksi ke database
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql', // atau 'postgres', 'sqlite', tergantung database kamu
+  }
+);
+
+// Tes koneksi (opsional)
+sequelize.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.error('Connection error:', err));
+
+module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const express=require('express')
-const app= express()
-const userRoutes = require('./routes/userRouter')
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRouter');
+require('dotenv').config();
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use('/user', userRoutes);
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server Running on PORT ${PORT} --> http://localhost:${PORT}`)
-})
+module.exports = app;
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+}
